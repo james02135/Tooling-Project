b9e81de061ab276bfa8840a8a51f2decd79bca78
james02135 <james02135@hotmail.com>
2021-04-06 15:42:20 +0100

    User Dashboard initialized, new project menu initialized
    Signed-off-by: james02135 <james02135@hotmail.com>

--- tooling_project/auth.py
+++ tooling_project/auth.py
@@ -1,14 +1,26 @@
-from flask import Blueprint, Flask, render_template, request, redirect, url_for, session, logging, flash
+from flask import (
+    Blueprint,
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    logging,
+    flash,
+)
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from .models import User
-from .forms import LoginForm, RegisterForm
+from .forms import *
 from . import db
 
-auth = Blueprint('auth', __name__)
+
+#All routes that require authentication
+auth = Blueprint("auth", __name__)
 
 
 # ----------------------------------------------------------------------------#
@@ -16,63 +28,75 @@ auth = Blueprint('auth', __name__)
 # ----------------------------------------------------------------------------#
 
 
-@auth.route('/login')
+@auth.route("/login")
 def login():
     form = LoginForm(request.form)
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form)
 
-@auth.route('/login', methods=['POST'])
+
+@auth.route("/login", methods=["POST"])
 def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    
-    #if this returns a user, then the email exists in the database
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # if this returns a user, then the email exists in the database
     user = User.query.filter_by(email=email).first()
 
     if not user or not check_password_hash(user.password, password):
-        flash('Please check you login details and try again')
-        return redirect(url_for('auth.login'))
+        flash("Please check you login details and try again")
+        return redirect(url_for("auth.login"))
 
-    #If both checks pass, this is an authenticated user
+    # If both checks pass, this is an authenticated user
     login_user(user)
-    return redirect(url_for('main.dashboard'))
+    return redirect(url_for("main.dashboard"))
 
-@auth.route('/register')
+
+@auth.route("/register")
 def register():
     form = RegisterForm(request.form)
-    return render_template('register.html', form=form)
+    return render_template("register.html", form=form)
 
-@auth.route('/register', methods=['POST'])
+
+@auth.route("/register", methods=["POST"])
 def register_post():
 
-    name = request.form.get('name')
-    ID = request.form.get('ID')
-    email = request.form.get('email')
-    password = request.form.get('password')
+    name = request.form.get("name")
+    ID = request.form.get("ID")
+    email = request.form.get("email")
+    password = request.form.get("password")
 
-    #check to see if the email is already being used
+    # check to see if the email is already being used
     user = User.query.filter_by(email=email).first()
-    
-    if user: #if that email is found, redirect the user back to the register form
-        flash('Email address already in use.')
-        return redirect(url_for('auth.register'))
-    
-    #create a new user, and hash the password
-    new_user = User(id = ID, name = name, email = email, password = generate_password_hash(password, method='sha256'))
-    
-    #add the new user to the database
+
+    if user:  # if that email is found, redirect the user back to the register form
+        flash("Email address already in use.")
+        return redirect(url_for("auth.register"))
+
+    # create a new user, and hash the password
+    new_user = User(
+        id=ID,
+        name=name,
+        email=email,
+        password=generate_password_hash(password, method="sha256"),
+    )
+
+    # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
-    return redirect(url_for('auth.login'))
-    
-@auth.route('/logout')
+    return redirect(url_for("auth.login"))
+
+
+@auth.route("/menu")
+def menu():
+    form = MenuForm(request.form)
+    return render_template("menu.html", form=form)
+
+
+
+
+
+@auth.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.home'))
-
-
-
-
-
-
+    return redirect(url_for("main.home"))
--- tooling_project/forms.py
+++ tooling_project/forms.py
@@ -2,17 +2,13 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Length
 
-
+# Class to set the register form with the required variables
 class RegisterForm(Form):
-    name = TextField(
-        "Name", validators=[DataRequired(), Length(min=6, max=25)]
-        )
+    name = TextField("Name", validators=[DataRequired(), Length(min=6, max=25)])
     ID = TextField(
         "Student ID Number", validators=[DataRequired(), Length(min=8, max=8)]
-        )
-    email = TextField(
-        "Email", validators=[DataRequired(), Length(min=6, max=40)]
-        )
+    )
+    email = TextField("Email", validators=[DataRequired(), Length(min=6, max=40)])
     password = PasswordField(
         "Password", validators=[DataRequired(), Length(min=6, max=40)]
     )
@@ -22,12 +18,12 @@ class RegisterForm(Form):
     )
 
 
+# Class to set the login form with the required variables
 class LoginForm(Form):
-    email = TextField(
-        "Email", [DataRequired()]
-        )
-    password = PasswordField(
-        "Password", [DataRequired()]
-        )
+    email = TextField("Email", [DataRequired()])
+    password = PasswordField("Password", [DataRequired()])
 
 
+# Class to set the new project menu form with the required variables
+class MenuForm(Form):
+    project_name = TextField("Project Name", [DataRequired()])
